4b27e011763ee5db6659d2d6ae1355c99c4dcee5
Chaiane Molaia <117865417+ChaianeMolaia@users.noreply.github.com>
2023-05-04 17:51:30 +0000

    commit inicial

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 
@@ -6,8 +7,14 @@ import { IonicModule } from '@ionic/angular';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule, CommonModule],
 })
 export class HomePage {
-  constructor() {}
+ produtos = [
+  {'id': 0, 'nome':'PS4', 'preco':2000}
+  {'id': 1, 'nome':'PS5', 'preco':5000}
+  {'id': 2, 'nome':'PS3', 'preco':1000}
+  {'id': 3, 'nome':'PS2', 'preco':500}
+  {'id': 4, 'nome':'PS1', 'preco':100}
+ ]
 }
