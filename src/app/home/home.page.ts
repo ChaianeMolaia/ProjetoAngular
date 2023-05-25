82abd7fcbd4a5292927b3a32a018fb8e2b2ab39b
Chaiane Molaia <117865417+ChaianeMolaia@users.noreply.github.com>
2023-05-25 17:42:55 +0000

    perfil

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -10,11 +10,5 @@ import { IonicModule } from '@ionic/angular';
   imports: [IonicModule, CommonModule],
 })
 export class HomePage {
- produtos = [
-  {'id': 0, 'nome':'PS4', 'preco':2000}
-  {'id': 1, 'nome':'PS5', 'preco':5000}
-  {'id': 2, 'nome':'PS3', 'preco':1000}
-  {'id': 3, 'nome':'PS2', 'preco':500}
-  {'id': 4, 'nome':'PS1', 'preco':100}
- ]
+  constructor() {}
 }
